f8fa2451acf53bebe85f12e3a9799f17f274e3a8
danackerson <dan@ackerson.de>
2020-12-18 14:53:49 +0100

    rest of seds

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,8 @@
 /sbin/syslogd && /usr/sbin/ipsec start
 
 sed -i -e "s/{{VPN_SERVER}}/$@.protonvpn.com/" /etc/swanctl/conf.d/protonvpn.conf
-
+sed -i -e "s/{{CTX_VPN_EAP_ID}}/$CTX_VPN_EAP_ID/g" /etc/swanctl/conf.d/protonvpn.conf
+sed -i -e "s@{{CTX_VPN_SECRET}}@$CTX_VPN_SECRET@" /etc/swanctl/conf.d/protonvpn.conf
 /bin/sleep 3
 /usr/sbin/swanctl -q && /usr/sbin/swanctl -i -c protonvpn
 
