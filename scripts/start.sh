225574aba7adf7df715268df19dd0a50b025caf9
danackerson <dan@ackerson.de>
2020-12-28 08:59:07 +0100

    use fqdn

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,10 +4,10 @@
 PROTONVPN_SERVER=$@
 if [ -z $@ ]
 then
-    PROTONVPN_SERVER=nl-10
+    PROTONVPN_SERVER=nl-10.protonvpn.com
 fi
 
-sed -i -e "s/{{VPN_SERVER}}/$PROTONVPN_SERVER.protonvpn.com/" /etc/swanctl/conf.d/protonvpn.conf
+sed -i -e "s/{{VPN_SERVER}}/$PROTONVPN_SERVER/" /etc/swanctl/conf.d/protonvpn.conf
 sed -i -e "s/{{CTX_VPN_EAP_ID}}/$CTX_VPN_EAP_ID/g" /etc/swanctl/conf.d/protonvpn.conf
 sed -i -e "s@{{CTX_VPN_SECRET}}@$CTX_VPN_SECRET@" /etc/swanctl/conf.d/protonvpn.conf
 sed -i -e "s@{{GITHUB_RUN_ID}}@$GITHUB_RUN_ID@" /root/.config/transmission-daemon/settings.json
