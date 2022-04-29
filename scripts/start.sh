9c49d8ba8f92bad17c49c3b6fd4ae9d7ecdf532f
danackerson <dan@ackerson.de>
2022-04-29 14:15:50 +0200

    need to start transmission

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,8 +10,9 @@ fi
 sed -i -e "s@{{GITHUB_RUN_ID}}@$GITHUB_RUN_ID@" /root/.config/transmission-daemon/settings.json
 sed -i -e "s@{{TRANSMISSION_HOST_NAME}}@$TRANSMISSION_HOST_NAME@" /root/.config/transmission-daemon/settings.json
 
-# ping -c 5 google.com # prime DNS
+ping -c 5 google.com # prime DNS
 /usr/bin/wg-quick up $PROTONVPN_SERVER
-/usr/bin/curl -4 https://ackerson.de/ip
+/usr/bin/curl https://ackerson.de/ip
+/usr/bin/transmission-daemon
 
 /usr/bin/tail -f /dev/null
