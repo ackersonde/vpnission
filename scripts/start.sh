901cf3709853acb5ec0c83c740084fa79014d5ed
Dan Ackerson <dan@ackerson.de>
2022-07-09 11:16:59 +0200

    Wireguard (#2)
    * create wireguard branch (doesn't work)
    
    * Revert "Merge branch 'main' of github.com:ackersonde/vpnission into wireguard"
    
    This reverts commit 789799282419f1e88f42e5cba0fd89c158769ca8, reversing
    changes made to 9eebe2b0f794a74abfa2863786a14021565ff72d.
    
    * add packet max size
    
    * build wireguard
    
    * fix notification msg, disable dashboard
    
    * default NL
    
    * add udp port
    
    * src_valid_mark
    
    * try MTU
    
    * upgrade traefik 2.8 [skip ci]
    
    * migrate to ORG secrets
    
    * migrate to ORG secrets
    
    * scp format
    
    * scp format
    
    * move hostname

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,13 @@
 #!/bin/ash
-/sbin/syslogd && /usr/sbin/ipsec start
+/sbin/syslogd
 
-PROTONVPN_SERVER=$@
-if [ -z $@ ]
-then
-    PROTONVPN_SERVER=nl-10.protonvpn.com
-fi
-
-sed -i -e "s/{{VPN_SERVER}}/$PROTONVPN_SERVER/" /etc/swanctl/conf.d/protonvpn.conf
-sed -i -e "s/{{CTX_VPN_EAP_ID}}/$CTX_VPN_EAP_ID/g" /etc/swanctl/conf.d/protonvpn.conf
-sed -i -e "s@{{CTX_VPN_SECRET}}@$CTX_VPN_SECRET@" /etc/swanctl/conf.d/protonvpn.conf
 sed -i -e "s@{{GITHUB_RUN_ID}}@$GITHUB_RUN_ID@" /root/.config/transmission-daemon/settings.json
 sed -i -e "s@{{TRANSMISSION_HOST_NAME}}@$TRANSMISSION_HOST_NAME@" /root/.config/transmission-daemon/settings.json
 
+/usr/bin/wg-quick up $PROTONVPN_SERVER
 ping -c 5 google.com # prime DNS
-/usr/sbin/swanctl -q && /usr/sbin/swanctl -i -c protonvpn
+
+/usr/bin/curl https://ackerson.de/ip
+/usr/bin/transmission-daemon
 
 /usr/bin/tail -f /dev/null
